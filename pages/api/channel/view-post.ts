e637ce9432d21087d3ae90d2b4a23d6a28b293a4
Stanley Winata <winatastanley355@gmail.com>
2022-10-03 10:06:23 +0700

    View post api

---
+++ pages/api/channel/view-post.ts
@@ -0,0 +1,20 @@
+import fetcher from "../../../lib/fetcher";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const channelHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const URL = `${process.env.CHANNEL_URL}/post/?slug=${req.query.slug}`;
+
+  let response: any;
+  try {
+    const res = await fetcher(URL, {});
+    console.log(res);
+    response = res.data;
+  } catch (err) {
+    response = err;
+  }
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(response);
+};
+
+export default channelHandler;
