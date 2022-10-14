35e8669d492a1bb356dd2adc8b470c9c7219014e
Stanley Winata <winatastanley355@gmail.com>
2022-10-14 16:44:22 +0700

    Subscription checker

--- components/pages/Channel/ChannelStatsBox/ChannelStatsBox.tsx
+++ components/pages/Channel/ChannelStatsBox/ChannelStatsBox.tsx
@@ -1,29 +1,17 @@
 import React from "react";
-import Link from "next/link";
-import useResponsive from "../../../../lib/hooks/useResponsive";
 import styles from "./ChannelStatsBox.module.scss";
 
 interface IChannelStats {
-  isMyChannel: boolean;
   stats: any;
 }
 
 const ChannelStatsBox = (props: IChannelStats) => {
-  const { isMyChannel, stats } = props;
-
-  const { isDesktop } = useResponsive();
+  const { stats } = props;
 
   return (
     <div className={styles.channel__stats}>
       <div className={styles.channel__stats__head}>
         <h1>{stats.channel_name}</h1>
-        {!isDesktop && !isMyChannel && (
-          <Link href={`/subscription/${stats.slug}`}>
-            <a title="Subscribe" className={styles.subscribe__cta}>
-              Subscribe
-            </a>
-          </Link>
-        )}
       </div>
       <div className={styles.channel__stats__numbers}>
         <span>
@@ -35,13 +23,6 @@ const ChannelStatsBox = (props: IChannelStats) => {
           <div>Subscribers</div>
         </span>
       </div>
-      {isDesktop && !isMyChannel && (
-        <Link href={`/subscription/${stats.slug}`}>
-          <a title="Subscribe" className={styles.subscribe__cta}>
-            Subscribe
-          </a>
-        </Link>
-      )}
     </div>
   );
 };
--- pages/channel/[slug].tsx
+++ pages/channel/[slug].tsx
@@ -1,28 +1,51 @@
 import React from "react";
+import Link from "next/link";
 import jwtDecode from "jwt-decode";
 import getConfig from "next/config";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import HasNoPostCard from "../../components/pages/Channel/HasNoPostCard/";
 import PostCard from "../../components/pages/Channel/PostCard";
 import ChannelStatsBox from "../../components/pages/Channel/ChannelStatsBox";
+import viewSubscription from "../../lib/subscriptionHandler/viewSubscription";
 import fetcher from "../../lib/fetcher";
 
 const { BASE_URL } = getConfig().publicRuntimeConfig;
 
 const ChannelSlug = (props: any) => {
-  const { slug, myChannel, channelStats, posts } = props;
+  const { slug, myChannel, channelStats, posts, lastSubcription } = props;
   const isMyChannel = myChannel && myChannel.slug === slug;
+
+  console.log(lastSubcription);
+
+  const SubscriptionLink = () => (
+    <div>
+      <div>Anda belum berlangganan Channel ini</div>
+      <div>Klik di bawah untuk mulai berlangganan</div>
+      <Link href={`/subscription/${slug}`}>
+        <a title={slug}>
+          Mulai Berlangganan
+        </a>
+      </Link>
+    </div>
+  );
+
+  const ChannelContent = () => {
+    if (posts.length > 0) {
+      if (lastSubcription && lastSubcription.paid) {
+        return <PostCard posts={posts} />
+      }
+      return <SubscriptionLink />
+    }
+
+    return <HasNoPostCard isMyChannel={isMyChannel} />
+  }
 
   return (
     <div className="container">
       {channelStats && (
-        <ChannelStatsBox stats={channelStats} isMyChannel={isMyChannel} />
+        <ChannelStatsBox stats={channelStats} />
       )}
-      {posts.length > 0 ? (
-        <PostCard posts={posts} />
-      ) : (
-        <HasNoPostCard isMyChannel={isMyChannel} />
-      )}
+      <ChannelContent />
     </div>
   );
 };
@@ -34,7 +57,10 @@ export const getServerSideProps: GetServerSideProps = async (
 
   const token = context.req.cookies["token"];
   const tokenChannel = context.req.cookies["token_channel"];
-  let myChannel;
+  let profile: any;
+  let myChannel: any;
+  let subscriptions;
+  let lastSubscription;
 
   if (!token) {
     return {
@@ -45,12 +71,27 @@ export const getServerSideProps: GetServerSideProps = async (
     };
   }
 
+  if (token) profile = jwtDecode(token);
   if (tokenChannel) myChannel = jwtDecode(tokenChannel);
+  const isMyChannel = myChannel && myChannel.slug === slug;
+
 
   const channelStats = await fetcher(
     `${BASE_URL}/api/channel/status/?slug=${slug}`,
     {}
   );
+
+  if (!isMyChannel && profile && channelStats && channelStats.data) {
+    subscriptions = await viewSubscription({
+      userID: profile.id,
+      channelID: channelStats.data.id,
+    });
+
+    if (subscriptions && subscriptions.length > 0) {
+      lastSubscription = subscriptions[subscriptions.length - 1];
+    }
+  }
+
   const posts = await fetcher(
     `${BASE_URL}/api/channel/view-post?slug=${slug}`,
     {}
@@ -62,6 +103,7 @@ export const getServerSideProps: GetServerSideProps = async (
       myChannel: myChannel ?? null,
       channelStats: channelStats?.data ?? null,
       posts: posts?.data ?? [],
+      lastSubcription: lastSubscription ?? null,
     },
   };
 };
