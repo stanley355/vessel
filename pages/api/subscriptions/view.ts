07fa5a8dbb3f585b29500fa00bb7e2cc13e0c2f9
Stanley Winata <winatastanley355@gmail.com>
2022-10-23 10:48:52 +0700

    View subscription

--- pages/api/subscriptions/view.ts
+++ pages/api/subscriptions/view.ts
@@ -1,8 +1,19 @@
 import fetcher from "../../../lib/fetcher";
+import { URLSearchParams } from "url";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const viewSubscriptionHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const URL = `${process.env.AUTHOR_URL}/subscriptions/`;
+const viewSubscriptionHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const params = {
+    user_id: req.query.user_id ?? "",
+    ...(req.query.channels_id && { channels_id: req.query.channels_id }),
+    ...(req.query.invoice_id && { invoice_id: req.query.invoice_id }),
+  };
+
+  const queryString = new URLSearchParams(params);
+  const URL = `${process.env.AUTHOR_URL}/subscriptions/?${queryString}`;
 
   const resp = await fetcher(URL, {
     method: "GET",
