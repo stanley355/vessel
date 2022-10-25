0b801c8ffbd3fa16767e32e7443e158cb342fab0
Stanley Winata <winatastanley355@gmail.com>
2022-10-25 09:52:31 +0700

    Free posts handler

--- pages/channel/[slug].tsx
+++ pages/channel/[slug].tsx
@@ -7,9 +7,11 @@ import ChannelNoPosts from "../../components/pages/Channel/ChannelNoPosts";
 import ChannelNotSubscribed from "../../components/pages/Channel/ChannelNotSubscribed";
 import SubscribeChannelForm from "../../components/pages/Channel/SubscribeChannelForm";
 import AwaitingPaymentForm from "../../components/pages/Channel/AwaitingPaymentForm";
-import findChannel from "../../lib/channelHandler/findChannel";
+import PostCard from "../../components/pages/Account/PostCard";
+import checkSubscriptionStatus from "../../lib/subscriptionHandler/checkSubscriptionStatus";
 import viewSubscriptions from "../../lib/subscriptionHandler/viewSubscriptions";
 import viewInvoice from "../../lib/paymentHandler/viewInvoice";
+import findChannel from "../../lib/channelHandler/findChannel";
 import fetcher from "../../lib/fetcher";
 import styles from "./ChannelSlug.module.scss";
 
@@ -18,39 +20,58 @@ const { BASE_URL } = getConfig().publicRuntimeConfig;
 interface IChannelSlug {
   profile: any;
   channel: any;
+  posts: any[];
   subscription: any;
   invoice: any;
 }
 
 const ChannelSlug = (props: IChannelSlug) => {
-  const { profile, channel, subscription, invoice } = props;
+  const { profile, channel, posts, subscription, invoice } = props;
 
   const [showSubscribeForm, setShowSubscribeForm] = useState(false);
 
+
+  const PostsSection = ({ postList }: any) => (
+    <div>
+      {postList.map((post: any) =>
+        <div key={post.id}>
+          <PostCard channel={channel} post={post} />
+        </div>
+      )}
+    </div>
+  )
+
+  const SubscriptionSection = () => {
+    if (showSubscribeForm) {
+      return <SubscribeChannelForm profile={profile} channel={channel} />
+    }
+    return subscription ? <AwaitingPaymentForm
+      profile={profile}
+      channelName={channel.channel_name}
+      subscriptionDuration={subscription.duration}
+      totalPrice={invoice.amount}
+      invoiceStatus={invoice.status}
+      invoiceLink={invoice.invoice_url}
+      onRenewClick={() => setShowSubscribeForm(true)}
+    /> : <ChannelNotSubscribed
+      onSubscribeClick={() => setShowSubscribeForm(true)}
+    />
+  }
+
   const ChannelBody = () => {
     if (channel && channel.posts_number > 0) {
-      if (subscription) {
-        return showSubscribeForm ? (
-          <SubscribeChannelForm profile={profile} channel={channel} />
-        ) : (
-          <AwaitingPaymentForm
-            profile={profile}
-            channelName={channel.channel_name}
-            subscriptionDuration={subscription.duration}
-            totalPrice={invoice.amount}
-            invoiceStatus={invoice.status}
-            invoiceLink={invoice.invoice_url}
-            onRenewClick={() => setShowSubscribeForm(true)}
-          />
-        );
+      const subscriptionStatus = checkSubscriptionStatus(subscription);
+
+      if (subscriptionStatus === "ONGOING") {
+        return <PostsSection postList={posts} />
       } else {
-        return showSubscribeForm ? (
-          <SubscribeChannelForm profile={profile} channel={channel} />
-        ) : (
-          <ChannelNotSubscribed
-            onSubscribeClick={() => setShowSubscribeForm(true)}
-          />
-        );
+        const freePosts = posts.filter((post: any) => post.is_free);
+        return (
+          <>
+            <SubscriptionSection />
+            {freePosts.length > 0 && <PostsSection postList={freePosts} />}
+          </>
+        )
       }
     } else {
       return <ChannelNoPosts />;
@@ -90,6 +111,8 @@ export const getServerSideProps: GetServerSideProps = async (
   const token = context.req.cookies["token"];
   const profile: any = token ? jwtDecode(token) : "";
   const channel = (await findChannel(slug)) ?? null;
+  const posts = await fetcher(`${BASE_URL}/api/channel/post/view?slug=${slug}`, {}) ?? [];
+
   let subscription;
   let invoice;
 
@@ -102,11 +125,11 @@ export const getServerSideProps: GetServerSideProps = async (
     subscription = subscriptionList[subscriptionList.length - 1];
   }
 
-  if (subscription && subscription.invoice_id) {
+  if (subscription && !subscription.paid && !subscription.expired_at) {
     invoice = await viewInvoice(subscription.invoice_id);
   }
 
-  // const posts = await fetcher(`${BASE_URL}/api/channel/post/view?slug=${slug}`, {});
+
 
   if (!token) {
     return {
@@ -121,6 +144,7 @@ export const getServerSideProps: GetServerSideProps = async (
     props: {
       profile,
       channel,
+      posts,
       subscription: subscription ?? null,
       invoice: invoice ?? null,
     },
