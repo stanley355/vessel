ae6fd01050d739eccb9d1dc689070554415c1862
Stanley Winata <winatastanley355@gmail.com>
2022-11-01 16:54:58 +0700

    Create paymetn function

---
+++ lib/paymentHandler/createPayment.ts
@@ -0,0 +1,24 @@
+import getConfig from "next/config";
+import fetcher from "../fetcher";
+
+interface ICreatePayment {
+  channelID: number;
+  channelName: string;
+  subscriberID: string;
+  subscriberName: string;
+  subscriptionDuration: number;
+  totalAmount: number;
+}
+
+const { BASE_URL } = getConfig().publicRuntimeConfig;
+
+const createPayment = async (payload: ICreatePayment ) => {
+  const paymentRes = await fetcher(`${BASE_URL}/api/payment/`, {
+    method: "POST",
+    data: payload,
+  });
+
+  return paymentRes;
+};
+
+export default createPayment;
---
+++ pages/api/payment/index.ts
@@ -0,0 +1,19 @@
+import fetcher from "../../../lib/fetcher";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const paymentHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const URL = `${process.env.PAYMENT_URL}/payment/`;
+
+  const resp = await fetcher(URL, {
+    method: req.method,
+    headers: {
+      Authorization: process.env.PAYMENT_API_TOKEN,
+    },
+    data: JSON.stringify(req.body),
+  });
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(resp);
+};
+
+export default paymentHandler;
