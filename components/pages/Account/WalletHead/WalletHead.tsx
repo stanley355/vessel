91f114274c4dd7a7039f49797cfabf2251a6fa35
Stanley Winata <winatastanley355@gmail.com>
2022-11-01 13:49:04 +0700

    Wallet head

--- components/pages/Account/WalletHead/WalletHead.tsx
+++ components/pages/Account/WalletHead/WalletHead.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { FaWallet } from 'react-icons/fa';
 import styles from './WalletHead.module.scss';
 
-const WalletHead = () => {
+interface IWalletHead {
+  balance: any;
+}
+
+const WalletHead = (props: IWalletHead) => {
+  const { balance } = props;
+
   return (
     <div className={styles.wallet__head}>
       <span>
         <FaWallet />
         <div className={styles.info}>
           <div>My Wallet</div>
-          <div>RP 0</div>
+          <div>{balance && balance.length > 0 ? `Rp ${balance[0].amount}` : "Terjadi kesalahan saat pengambilan data"}</div>
         </div>
       </span>
 
--- pages/account/wallet.tsx
+++ pages/account/wallet.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
+import jwtDecode from 'jwt-decode';
+import viewBalance from '../../lib/paymentHandler/viewBalance';
 import WalletHead from '../../components/pages/Account/WalletHead';
 
-const Wallet = () => {
+const Wallet = (props: any) => {
+  const { balance } = props;
+
   return (
     <div className='container'>
-      <WalletHead />
+      <WalletHead balance={balance} />
     </div>
   )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = context.req.cookies["token"];
+  const profile: any = token ? jwtDecode(token) : "";
+  let balance: any;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/account/login/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (profile && profile.id) {
+    balance = await viewBalance(profile.id);
+  }
+
+  return {
+    props: {
+      profile: profile ?? null,
+      balance: balance ?? null,
+    },
+  };
+};
 
 export default Wallet;
