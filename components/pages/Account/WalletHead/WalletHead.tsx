fe90b7a43410e0ff23416bdf4e6c1105794c56dc
Stanley Winata <winatastanley355@gmail.com>
2022-11-01 13:50:06 +0700

    prettier

--- components/pages/Account/WalletHead/WalletHead.tsx
+++ components/pages/Account/WalletHead/WalletHead.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FaWallet } from 'react-icons/fa';
-import styles from './WalletHead.module.scss';
+import React from "react";
+import { FaWallet } from "react-icons/fa";
+import styles from "./WalletHead.module.scss";
 
 interface IWalletHead {
   balance: any;
@@ -15,13 +15,17 @@ const WalletHead = (props: IWalletHead) => {
         <FaWallet />
         <div className={styles.info}>
           <div>My Wallet</div>
-          <div>{balance && balance.length > 0 ? `Rp ${balance[0].amount}` : "Terjadi kesalahan saat pengambilan data"}</div>
+          <div>
+            {balance && balance.length > 0
+              ? `Rp ${balance[0].amount}`
+              : "Terjadi kesalahan saat pengambilan data"}
+          </div>
         </div>
       </span>
 
-      <button type='button'>Withdraw</button>
+      <button type="button">Withdraw</button>
     </div>
-  )
-}
+  );
+};
 
 export default WalletHead;
--- pages/account/wallet.tsx
+++ pages/account/wallet.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { GetServerSideProps } from 'next';
-import jwtDecode from 'jwt-decode';
-import viewBalance from '../../lib/paymentHandler/viewBalance';
-import WalletHead from '../../components/pages/Account/WalletHead';
+import React from "react";
+import { GetServerSideProps } from "next";
+import jwtDecode from "jwt-decode";
+import viewBalance from "../../lib/paymentHandler/viewBalance";
+import WalletHead from "../../components/pages/Account/WalletHead";
 
 const Wallet = (props: any) => {
   const { balance } = props;
 
   return (
-    <div className='container'>
+    <div className="container">
       <WalletHead balance={balance} />
     </div>
-  )
-}
-
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const token = context.req.cookies["token"];
