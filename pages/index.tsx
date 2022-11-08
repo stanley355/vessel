2ab9b8e53be71fc8f07d90031876acbd77b3df40
Stanley Winata <winatastanley355@gmail.com>
2022-11-08 18:53:20 +0700

    Homepage fetch posts data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
+import React from 'react';
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import jwtDecode from 'jwt-decode';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import viewHomePosts from '../lib/postHandler/viewHomePosts';
+import viewSubscriptions from '../lib/subscriptionHandler/viewSubscriptions';
+import filterSimilarSubscription from '../lib/filterSimilarSubscription';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
+  const { posts } = props;
   return <div className="container">hi</div>;
 };
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+
+  const token = context.req.cookies["token"];
+  const profile: any = token ? jwtDecode(token) : "";
+  let posts: any[] = [];
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/account/login/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (profile && profile.id) {
+    const rawSubscriptions = await viewSubscriptions({ userID: profile.id });
+    if (rawSubscriptions && rawSubscriptions.length > 0) {
+      const subscriptions = filterSimilarSubscription(rawSubscriptions);
+      const subscriptionsChannelID = subscriptions.map((subscription) => Number(subscription.channels_id));
+      posts = await viewHomePosts(subscriptionsChannelID);
+    } else {
+      posts = await viewHomePosts([]);
+    }
+  }
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default Home;
