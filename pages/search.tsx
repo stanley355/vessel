865a415e0766000d079e9b007872ae27c25ab0be
Stanley Winata <winatastanley355@gmail.com>
2022-11-13 09:14:53 +0700

    Search page functionality

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { FaSearch } from 'react-icons/fa';
+import useResponsive from '../lib/hooks/useResponsive';
+import searchSimilarChannel from '../lib/channelHandler/searchSimilarChannel';
+import styles from '../styles/pages/search.module.scss';
 
 const SearchPage = () => {
+  const { isDesktop } = useResponsive();
+
+  const [similarChannels, setSimilarChannels] = useState([]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    
+    if (e.target.channelName.value.length > 0) {
+      const channels = await searchSimilarChannel(e.target.value);
+
+      if (channels && channels.length > 0) {
+        setSimilarChannels(channels);
+      }
+    } else {
+      setSimilarChannels([]);
+    }
+  }
+
+  const handleInputOnchange = (e: any) => {
+    setTimeout(async () => {
+      if (e.target.value.length > 0) {
+        const channels = await searchSimilarChannel(e.target.value);
+
+        if (channels && channels.length > 0) {
+          setSimilarChannels(channels);
+        }
+      } else {
+        setSimilarChannels([]);
+      }
+    }, 1000);
+  }
+
   return (
     <div className='container'>
-      <div>
-        <form action="">
-          <input type="text" placeholder='Search your favourite Channel' />
-          <button type='submit'> Search</button>
+      <div className={styles.search__page}>
+        <h3 className={styles.title}>Search for your favourite Channel</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="channelName" placeholder='Search...' onChange={handleInputOnchange} />
+          <button type='submit'> <FaSearch /> {isDesktop && 'Search'}</button>
         </form>
+        <div className={styles.results}>
+          {similarChannels.length > 0 && similarChannels.map((channel: any) =>
+            <Link href={`/channel/${channel.slug}`} key={channel.channel_name}>
+              <div className={styles.channel__link}>
+                <div className={styles.channel__img}>
+                  <img src={channel.profile_img_url} alt={channel.channel_name} />
+                </div>
+                {channel.channel_name}
+              </div>
+            </Link>
+          )}
+        </div>
       </div>
 
     </div>
@@ -17,9 +66,3 @@ const SearchPage = () => {
 }
 
 export default SearchPage;
-
-export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-  return {
-    props: {}
-  }
-}
