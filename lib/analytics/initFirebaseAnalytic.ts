63ac33b4da287d3af62d448f9bb6a6b31d7faf56
Stanley Winata <winatastanley355@gmail.com>
2022-11-16 10:21:24 +0700

    Fix firebase

--- lib/analytics/initFirebaseAnalytic.ts
+++ lib/analytics/initFirebaseAnalytic.ts
@@ -3,10 +3,10 @@ import fetcher from "../fetcher";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const { KONTENKU_URL } = getConfig().publicRuntimeConfig;
+const { BASE_URL } = getConfig().publicRuntimeConfig;
 
 const initFirebaseAnalytic = async () => {
-  const configURL = `${KONTENKU_URL}/api/firebase-config/`;
+  const configURL = `${BASE_URL}/api/firebase-config/`;
   const firebaseConfig: any = await fetcher(configURL, {});
 
   const app = initializeApp(firebaseConfig);
