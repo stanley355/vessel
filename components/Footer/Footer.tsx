aeaa68842a9e36143bcaba709e16e28feec9807f
Stanley Winata <winatastanley355@gmail.com>
2022-11-25 13:03:08 +0700

    Remove suport from footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaRegCopyright, FaPaperPlane } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
@@ -8,9 +8,6 @@ const Footer = () => {
       <div className={styles.footer}>
         <div>
           Copyright <FaRegCopyright /> {new Date().getFullYear()} Kontenku
-        </div>
-        <div>
-          Support <FaPaperPlane /> winatastanley355@gmail.com / 083807275627 / GG. Langgar no 21
         </div>
       </div>
     </div>
