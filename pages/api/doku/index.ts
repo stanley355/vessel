6875cf170f77cfe026a0f302f8500e432883e3e3
Stanley Winata <winatastanley355@gmail.com>
2022-12-02 08:35:13 +0700

    Doku va

--- pages/api/doku/index.ts
+++ pages/api/doku/index.ts
@@ -8,7 +8,7 @@ const generateSignatureKey = (payload: any) => {
   const clientID = `Client-Id:BRN-0263-1669701997867`;
   const requestID = `Request-Id:kontenku-${new Date().toISOString()}`;
   const requestTimestamp = `Request-Timestamp:${new Date().toISOString()}`;
-  const reqTarget = "Request-Target:/doku-virtual-account/v2/payment-code";
+  const reqTarget = "Request-Target:/bri-virtual-account/v2/payment-code";
   const hashDigest = sha256(payload);
   const hmacDigest = Base64.stringify(hashDigest);
   const digest = `Digest:${hmacDigest}`;
@@ -33,7 +33,7 @@ const generateSignatureKey = (payload: any) => {
 };
 
 const dokuHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const URL = "https://api.doku.com/doku-virtual-account/v2/payment-code/";
+  const URL = "https://api.doku.com/bri-virtual-account/v2/payment-code/";
 
   const resp = await fetcher(URL, {
     method: "POST",
