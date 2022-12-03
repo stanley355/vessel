107e8907a4931a244e15ed579a17fc93865fe22b
Stanley Winata <winatastanley355@gmail.com>
2022-12-03 15:06:41 +0700

    handle accepted withdrawal

--- components/pages/Admin/OngoingWithdrawalTable/OngoingWithdrawalTable.tsx
+++ components/pages/Admin/OngoingWithdrawalTable/OngoingWithdrawalTable.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
+import Router from 'next/router';
+import getConfig from 'next/config';
+import fetcher from '../../../../lib/fetcher';
+import handleAcceptedWithdrawal from '../../../../lib/withdrawalHandler/handleAcceptedWithdrawal';
+import styles from './OngoingWithdrawalTable.module.scss';
 
 interface IWithdrawalTable {
   ongoingWithdrawals: any[]
 }
 
+const { KONTENKU_URL } = getConfig().publicRuntimeConfig;
+
 const OngoingWithdrawalTable = (props: IWithdrawalTable) => {
   const { ongoingWithdrawals } = props;
 
-  //   account_number
-  // account_owner_name
-  // amount
-  // bank_name
-  // created_at
-  // id
-  // status
-  // updated_at
-  // user_id
-  console.log(ongoingWithdrawals);
+
   return (
-    <div>
+    <div className={styles.withdrawal__table}>
       <table>
         <thead>
           <tr>
@@ -36,7 +34,7 @@ const OngoingWithdrawalTable = (props: IWithdrawalTable) => {
         <tbody>
           {ongoingWithdrawals.length > 0 &&
             ongoingWithdrawals.map((withdrawal: any) =>
-              <tr>
+              <tr key={withdrawal.id}>
                 <td>{withdrawal.id}</td>
                 <td>{withdrawal.user_id}</td>
                 <td>{new Date(withdrawal.created_at).toDateString()}</td>
@@ -45,8 +43,8 @@ const OngoingWithdrawalTable = (props: IWithdrawalTable) => {
                 <td>{withdrawal.account_owner_name}</td>
                 <td>{withdrawal.amount}</td>
                 <td>{withdrawal.status}</td>
-                <td>
-                  <button>Accept</button>
+                <td className={styles.action__column}>
+                  <button onClick={()=> handleAcceptedWithdrawal(withdrawal)  }>Accept</button>
                   <button>Reject</button>
                 </td>
               </tr>
---
+++ lib/withdrawalHandler/handleAcceptedWithdrawal.ts
@@ -0,0 +1,26 @@
+import getConfig from "next/config";
+import Router from "next/router";
+import fetcher from "../fetcher";
+
+const { KONTENKU_URL } = getConfig().publicRuntimeConfig;
+
+const handleAcceptedWithdrawal = async (withdrawal: any) => {
+  const URL = `${KONTENKU_URL}/api/payment/withdrawal/`;
+  const withdrawalRes = await fetcher(URL, {
+    method: "PUT",
+    data: {
+      withdrawalID: withdrawal.id,
+      status: "SUCCESS",
+      message: `Telah di transfer pada ${new Date().toLocaleString()}`,
+    },
+  });
+
+  if (withdrawalRes.status === "SUCCESS") {
+    Router.reload();
+  } else {
+    alert("Withdrawal Update fail");
+    return "";
+  }
+};
+
+export default handleAcceptedWithdrawal;
