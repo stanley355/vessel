d88124f24d347825cbfe72a80aac24a4f62b9e41
Stanley Winata <winatastanley355@gmail.com>
2022-12-03 14:16:04 +0700

    Adming login form functionality

--- components/pages/Admin/AdminLoginForm/AdminLoginForm.tsx
+++ components/pages/Admin/AdminLoginForm/AdminLoginForm.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import getConfig from 'next/config';
+import Router from 'next/router';
+import CryptoJS from 'crypto-js';
+import Cookies from 'js-cookie';
+import fetcher from '../../../../lib/fetcher';
 import styles from './AdminLoginForm.module.scss';
 
-const AdminLoginForm = () => {
+const { BASE_URL, ADMIN_CRYPTO_SECRET } = getConfig().publicRuntimeConfig;
 
-  const handleSubmit = (e:any) => {
+const AdminLoginForm = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const {email, password} = e.target;
-    console.log(email.value);
+    const { email, password } = e.target;
+
+    const URL = `${BASE_URL}/api/payment/withdrawal/admin/`;
+    const admin = await fetcher(URL, {});
+
+    const bytes = CryptoJS.AES.decrypt(admin, ADMIN_CRYPTO_SECRET);
+    const decrypt = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+    if ((email.value === decrypt.email) && (password.value === decrypt.password)) {
+      const fifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
+      Cookies.set("token_admin", admin, { expires: fifteenMinutes });
+      Router.reload();
+    } else {
+      setErrorMsg("Not Admin");
+      return ""
+    }
   }
 
   return (
     <div className={styles.login__form}>
       <h3>R u Admin ?</h3>
-      <form onSubmit={handleSubmit}> 
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Admin Email' name='email' />
-        <input type="passowrd" placeholder='password' name='password'/>
+        <input type="password" placeholder='password' name='password' />
         <button type="submit">Submit?</button>
       </form>
+      {errorMsg && <span>{errorMsg}</span>}
     </div>
   )
 }
--- pages/api/payment/withdrawal/admin.ts
+++ pages/api/payment/withdrawal/admin.ts
@@ -1,19 +1,20 @@
+import CryptoJS from "crypto-js";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const withdrawalAdminHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+const withdrawalAdminHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
 
-  if (req.headers.authorization === process.env.PAYMENT_API_TOKEN) {
-    const admin = {
-      email: process.env.ADMIN_EMAIL,
-      password: process.env.ADMIN_PASSWORD,
-    };
+  const admin = {
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+  };
 
-    res.setHeader("Content-Type", "application/json");
-    res.json(admin);
-  } else {
-    res.setHeader("Content-Type", "application/json");
-    res.json({});
-  }
+  const encpyted = CryptoJS.AES.encrypt(JSON.stringify(admin), process.env.ADMIN_CRYPTO_SECRET ?? "").toString();
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(encpyted);
 };
 
 export default withdrawalAdminHandler;
