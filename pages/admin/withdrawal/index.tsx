1f8e8690fc519f1fa288e8f7a56a2747c6d04f72
Stanley Winata <winatastanley355@gmail.com>
2022-12-03 11:38:34 +0700

    Ongoing withdrawal handler

---
+++ pages/admin/withdrawal/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import findAllOngoingWithdrawal from '../../../lib/withdrawalHandler/findAllOngoingWithdrawal';
+
+const WithdrawalPage = (props: any) => {
+  const { ongoingWithdrawals } = props;
+
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const ongoingWithdrawals = await findAllOngoingWithdrawal() ?? [];
+
+
+  return {
+    props: {
+      ongoingWithdrawals: ongoingWithdrawals ?? []
+    }
+  }
+}
+
+export default WithdrawalPage;
