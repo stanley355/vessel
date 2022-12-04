0567cab0d9713605dd29f1356f0e2d6bf3abf3d9
Stanley Winata <winatastanley355@gmail.com>
2022-12-04 16:50:38 +0700

    Checkout page server props

--- pages/checkout/[order_id].tsx
+++ pages/checkout/[order_id].tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
 const CheckoutPage = () => {
   return(
-    <div>
+    <div className='container'>
       <h1>hi</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+
+  const orderID = context.query.order_id ?? "";
+  
+  console.log(orderID);
+  return {
+    props: {}
+  }
 }
 
 export default CheckoutPage;
