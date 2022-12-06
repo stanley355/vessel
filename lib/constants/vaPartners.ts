92ea608d8bb691af2ed4d55388f1f8324482f2a3
Stanley Winata <winatastanley355@gmail.com>
2022-12-06 09:09:44 +0700

    Add partners asset

---
+++ lib/constants/vaPartners.ts
@@ -0,0 +1,38 @@
+export const VIRTUAL_ACCOUNT_PARTNERS = [
+  {
+    label: "BCA via Doku",
+    value: "BCA",
+  },
+  {
+    label: "Bank BNI",
+    value: "BNI",
+  },
+  {
+    label: "Bank BRI",
+    value: "BRI",
+  },
+  {
+    label: "Bank Syariah Indonesia",
+    value: "BSI",
+  },
+  {
+    label: "Bank Mandiri",
+    value: "MANDIRI",
+  },
+  {
+    label: "Bank CIMB",
+    value: "CIMB",
+  },
+  {
+    label: "Doku Virtual Account",
+    value: "DOKU",
+  },
+  {
+    label: "Bank Danamon",
+    value: "DANAMON",
+  },
+  {
+    label: "Bank Permata",
+    value: "PERMATA",
+  },
+];
--- pages/checkout/[order_id].tsx
+++ pages/checkout/[order_id].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import getConfig from 'next/config';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import jwtDecode from 'jwt-decode';
+import Select from 'react-select'
 import fetcher from '../../lib/fetcher';
 import styles from './checkout.module.scss';
 
@@ -11,22 +12,41 @@ const { KONTENKU_URL } = getConfig().publicRuntimeConfig;
 const CheckoutPage = (props: any) => {
   const { profile, channel, order } = props;
 
-  console.log(order);
+  //   {
+  //   id: 'a22d23ab-8002-4f87-baeb-c56cdb899c3b',
+  //   created_at: '2022-12-06T01:26:11.000Z',
+  //   updated_at: '2022-12-06T01:26:11.000Z',
+  //   expired_at: '2022-12-06T01:26:11.000Z',
+  //   channel_id: 8,
+  //   subscriber_id: '3586a3c5-a8c3-42f4-92f8-a9a940738802',
+  //   subscription_duration: 1,
+  //   amount: 15000,
+  //   merchant: null,
+  //   merchant_order_id: null,
+  //   merchant_payment_link: null,
+  //   status: 'PENDING',
+  //   merchant_va_number: null
+  // }
+
   return (
     <div className='container'>
-      <div className='box'>
-        {/* <div>
+      <div className={styles.checkout}>
+        <div className={styles.logo__wrap}>
           <img src="/images/kontenku-logo-short.png" alt="kontenku" />
-        </div> */}
-        <div>
-          <h3>Pembayaran langganan channel: {channel}</h3>
+        </div>
+        <div className={styles.info}>
+          <h3>Subscription channel: {channel}</h3>
           <div>Order ID: {order.id}</div>
-          <div className={styles.info}>Nama pelanggan : {profile.fullname} </div>
-          <div className={styles.info}>Email : {profile.email} </div>
-          <div className={styles.info}>
+          <div >Nama pelanggan : {profile.fullname} </div>
+          <div >Email : {profile.email} </div>
+          <div >
             Durasi Langganan: {order.subscription_duration} Bulan
           </div>
-          <div className={styles.info}>Total Harga: {order.amount}</div>
+          <div >Total Harga: {order.amount}</div>
+        </div>
+
+        <div>
+          
         </div>
       </div>
     </div>
