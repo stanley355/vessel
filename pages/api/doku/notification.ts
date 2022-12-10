80cbbc55331b8be3a4774c5e30b81a81b912d6ca
Stanley Winata <winatastanley355@gmail.com>
2022-12-10 14:32:13 +0700

    add log

--- pages/api/doku/notification.ts
+++ pages/api/doku/notification.ts
@@ -6,6 +6,7 @@ const dokuNotificationHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  console.log(111, req.body);
   await NextCors(req, res, {
     methods: ["POST"],
     origin: "*",
@@ -14,30 +15,38 @@ const dokuNotificationHandler = async (
 
   let response = {};
 
-  const orderURL = `${process.env.PAYMENT_URL}/v2/order/paid?orderID=${req.body.order.invoice_number}`;
 
-  const orderRes = await fetcher(orderURL, {
-    method: "PUT",
-    headers: {
-      Authorization: process.env.PAYMENT_API_TOKEN,
-    },
-    data: JSON.stringify(req.body),
-  });
+    console.log(222, req.body);
+  if (req.body && req.body.order) {
+    const orderURL = `${process.env.PAYMENT_URL}/v2/order/paid?orderID=${req.body.order.invoice_number}`;
 
-  if (orderRes && orderRes.id) {
-    const authorURL = `${process.env.AUTHOR_URL}/subscriptions/`;
-
-    const authorPayload = {
-      user_id: orderRes.subscriber_id,
-      channels_id: orderRes.channel_id,
-      duration: orderRes.subscription_duration,
-    };
-    response = await fetcher(authorURL, {
-      method: "POST",
-      data: JSON.stringify(authorPayload),
+    const orderRes = await fetcher(orderURL, {
+      method: "PUT",
+      headers: {
+        Authorization: process.env.PAYMENT_API_TOKEN,
+      },
+      data: JSON.stringify(req.body),
     });
-  }
 
+
+    console.log(333, orderRes);
+    if (orderRes && orderRes.id) {
+      const authorURL = `${process.env.AUTHOR_URL}/subscriptions/`;
+
+      const authorPayload = {
+        user_id: orderRes.subscriber_id,
+        channels_id: orderRes.channel_id,
+        duration: orderRes.subscription_duration,
+      };
+      response = await fetcher(authorURL, {
+        method: "POST",
+        data: JSON.stringify(authorPayload),
+      });
+    }
+
+
+    console.log(444, response);
+  }
   res.setHeader("Content-Type", "application/json");
   res.json(response);
 };
