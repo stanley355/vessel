35e40c9665a05e045e64f58132a61809a6153a1a
Stanley Winata <winatastanley355@gmail.com>
2022-12-11 10:51:15 +0700

    Pending order api

---
+++ pages/api/payment/order/pending.ts
@@ -0,0 +1,24 @@
+import fetcher from "../../../../lib/fetcher";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const pendingOrderHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const URL = `${process.env.PAYMENT_URL}/v2/order/pending/${
+    req.query.orderID && `?orderID=${req.query.orderID}`
+  }`;
+
+  const resp = await fetcher(URL, {
+    method: req.method,
+    headers: {
+      Authorization: process.env.PAYMENT_API_TOKEN,
+    },
+    data: JSON.stringify(req.body),
+  });
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(resp);
+};
+
+export default pendingOrderHandler;
