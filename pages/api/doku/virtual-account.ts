5ebdff340c43e4e70614833966ba58c7f62f11df
Stanley Winata <winatastanley355@gmail.com>
2023-01-02 08:13:59 +0700

    generate doku signature key

--- pages/api/doku/virtual-account.ts
+++ pages/api/doku/virtual-account.ts
@@ -3,41 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import sha256 from "crypto-js/sha256";
 import hmacSHA256 from "crypto-js/hmac-sha256";
 import Base64 from "crypto-js/enc-base64";
+import generateDokuSignatureKey from "../../../lib/doku/generateDokuSignatureKey";
 
-interface IReqBody {
-  order_id: string;
-  doku_va_path: string;
-  doku_payload: any;
-}
-
-const generateDokuSignatureKey = (payload: IReqBody) => {
-  const clientID = `Client-Id:${process.env.DOKU_CLIENT_ID}`;
-  const reqID = `Request-Id:${payload.order_id}`;
-  const reqTimestamp = `Request-Timestamp:${new Date().toISOString()}`;
-  const reqTarget = `Request-Target:${payload.doku_va_path}`;
-
-  const hashDigest = sha256(payload.doku_payload);
-  const hmacDigest = Base64.stringify(hashDigest);
-  const digest = `Digest:${hmacDigest}`;
-
-  const signatureString =
-    clientID +
-    "\n" +
-    reqID +
-    "\n" +
-    reqTimestamp +
-    "\n" +
-    reqTarget +
-    "\n" +
-    digest;
-
-  const privateKey = process.env.DOKU_SECRET_KEY ?? "";
-  const hmacSignature = Base64.stringify(
-    hmacSHA256(signatureString, privateKey)
-  );
-
-  return hmacSignature;
-};
 
 const dokuVAHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const URL = `${process.env.DOKU_API_URL}${req.body.doku_va_path}`;
@@ -55,6 +22,7 @@ const dokuVAHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     data: JSON.stringify(req.body.doku_payload),
   });
 
+  console.log(333, resp);
   res.setHeader("Content-Type", "application/json");
   res.json(resp);
 };
