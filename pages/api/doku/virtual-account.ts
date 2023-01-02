5c8d7c4aadd9b1af018ce10c2c8755a307b28035
Stanley Winata <winatastanley355@gmail.com>
2023-01-02 08:31:48 +0700

    Remove unused code

--- pages/api/doku/virtual-account.ts
+++ pages/api/doku/virtual-account.ts
@@ -1,10 +1,6 @@
 import fetcher from "../../../lib/fetcher";
 import { NextApiRequest, NextApiResponse } from "next";
-import sha256 from "crypto-js/sha256";
-import hmacSHA256 from "crypto-js/hmac-sha256";
-import Base64 from "crypto-js/enc-base64";
 import generateDokuSignatureKey from "../../../lib/doku/generateDokuSignatureKey";
-
 
 const dokuVAHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const URL = `${process.env.DOKU_API_URL}${req.body.doku_va_path}`;
@@ -22,7 +18,6 @@ const dokuVAHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     data: JSON.stringify(req.body.doku_payload),
   });
 
-  console.log(333, resp);
   res.setHeader("Content-Type", "application/json");
   res.json(resp);
 };
