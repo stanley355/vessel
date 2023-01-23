515eb731bcd946f2602106fe8d2a4b147ddfaab5
Stanley Winata <winatastanley355@gmail.com>
2023-01-23 10:12:46 +0700

    Accont login hero component

--- pages/account/login/index.tsx
+++ pages/account/login/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from "next";
 import React from "react";
 import getConfig from "next/config";
 import fetcher from "../../../lib/fetcher";
-import GoogleSignInBtn from "../../../components/GoogleSignInBtn";
+import AccountLoginHero from "../../../components/pages/Account/AccountLoginHero/AccountLoginHero";
 import HomeMetaHead from "../../../components/pages/Home/HomeMetaHead";
 import styles from "./login.module.scss";
 
@@ -14,20 +14,7 @@ const AccountLogin = (props: any) => {
   return (
     <div>
       <HomeMetaHead posts={[]} />
-      <div className={styles.account__login}>
-        <div className="container">
-          <div className={styles.copywriting}>
-            <div className={styles.logo__wrap}>
-              <img src="/images/kontenku-logo-short.png" alt="Kontenku" width={360} height={150} />
-            </div>
-            <div className={styles.subtitle}>Exclusive for the Fans</div>
-          </div>
-          <div className={styles.text}>Masuk / Daftar</div>
-          <div className={styles.gsignin__wrap}>
-            <GoogleSignInBtn clientID={clientID} />
-          </div>
-        </div>
-      </div>
+      <AccountLoginHero clientID={clientID} />
     </div>
   );
 };
