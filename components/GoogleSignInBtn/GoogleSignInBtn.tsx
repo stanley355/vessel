5319a6d73de3573e2c6b6087d574daa804a2649e
Stanley Winata <winatastanley355@gmail.com>
2023-01-24 08:50:47 +0700

    Login page design revamp

--- components/GoogleSignInBtn/GoogleSignInBtn.tsx
+++ components/GoogleSignInBtn/GoogleSignInBtn.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import gmailLoginHandler from "../../lib/loginHandler/gmailLoginHandler";
+import useResponsive from "../../lib/hooks/useResponsive";
 import { WARNING_MSG } from "../../lib/warning-messages";
 
 interface IGoogleSignIn {
@@ -9,6 +10,8 @@ interface IGoogleSignIn {
 
 const GoogleSignInBtn = (props: IGoogleSignIn) => {
   const { clientID } = props;
+
+  const { isDesktop } = useResponsive();
 
   return (
     <GoogleOAuthProvider clientId={clientID}>
--- pages/account/login/index.tsx
+++ pages/account/login/index.tsx
@@ -4,6 +4,7 @@ import getConfig from "next/config";
 import Router from "next/router";
 import Cookies from "js-cookie";
 import fetcher from "../../../lib/fetcher";
+import useResponsive from "../../../lib/hooks/useResponsive";
 import AccountLoginHero from "../../../components/pages/Account/AccountLoginHero";
 import HomeMetaHead from "../../../components/pages/Home/HomeMetaHead";
 import GoogleSignInBtn from "../../../components/GoogleSignInBtn";
@@ -14,6 +15,8 @@ const { BASE_URL } = getConfig().publicRuntimeConfig;
 const AccountLogin = (props: any) => {
   const { clientID } = props;
 
+  const { isDesktop } = useResponsive();
+
   useEffect(() => {
     const token = Cookies.get('token');
     if (token) {
@@ -21,38 +24,56 @@ const AccountLogin = (props: any) => {
     }
   }, [clientID]);
 
+  const KontenkuInfo = () => {
+    return (
+      <div className={styles.info}>
+        <div className={isDesktop ? "container" : ""}>
+          <div className={styles.info__content}>
+            <div className={styles.head__container}>
+              <div className={styles.head}>
+                <h1 className={styles.title}>Apa itu Kontenku?</h1>
+                {!isDesktop &&
+                  <div>
+                    <img src="/images/home_hero.webp" alt="hero" />
+                  </div>}
+              </div>
+              <div className={styles.subtitle}>
+                Kontenku adalah platform konten berbasis subscription.
+                Creator dapat membuat content dan memasang harga
+                subscription untuk diakses oleh para fans. Setelah
+                pembayaran subscription diterima, kami akan mengirimkan
+                uang tersebut kepada creator dalam 1 hari kerja.
+              </div>
+            </div>
+            {isDesktop &&
+              <div>
+                <img src="/images/home_hero.webp" alt="hero" />
+              </div>}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.account__login}>
       <HomeMetaHead posts={[]} />
       <AccountLoginHero clientID={clientID} />
-      <div className={styles.info}>
-        <div className={styles.head}>
-          <h1 className={styles.title}>Apa itu Kontenku?</h1>
-          <div>
-            <img src="/images/home_hero.webp" alt="hero" />
-          </div>
-        </div>
-        <div className={styles.subtitle}>
-          Kontenku adalah platform konten berbasis subscription.
-          Creator dapat membuat content dan memasang harga
-          subscription untuk diakses oleh para fans. Setelah
-          pembayaran subscription diterima, kami akan mengirimkan
-          uang tersebut kepada creator dalam 1 hari kerja.
-        </div>
-      </div>
+      <KontenkuInfo />
       <div className={styles.benefit__container}>
-        <div className={styles.benefit}>
-          <h3 className={styles.title}>Keuntungan memulai di Kontenku</h3>
-
-          <ul>
-            <li>Bebas Biaya</li>
-            <li>Penarikan Uang Cepat</li>
-            <li>Penerimaan Uang Otomatis</li>
-            <li>Kustomisasi Konten</li>
-            <li>Penghasilan Konten Tambahan</li>
-            <li>Full Support untuk Creator</li>
-            <li>dan masih banyak lagi</li>
-          </ul>
+        <div className={isDesktop ? "container" : ""}>
+          <div className={styles.benefit}>
+            <h3 className={styles.title}>Keuntungan memulai di Kontenku</h3>
+            <ul>
+              <li>Bebas Biaya</li>
+              <li>Penarikan Uang Cepat</li>
+              <li>Penerimaan Uang Otomatis</li>
+              <li>Kustomisasi Konten</li>
+              <li>Penghasilan Konten Tambahan</li>
+              <li>Full Support untuk Creator</li>
+              <li>dan masih banyak lagi</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className={styles.invitation}>
