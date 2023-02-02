36e196e2b23de44a0b36f5e15c54d8a72584411e
Stanley Winata <winatastanley355@gmail.com>
2023-02-02 08:48:13 +0700

    UPdate seo

--- components/pages/Channel/ChannelMetaHead/ChannelMetaHead.tsx
+++ components/pages/Channel/ChannelMetaHead/ChannelMetaHead.tsx
@@ -10,8 +10,8 @@ const ChannelMetaHead = (props: IChannelMetaHead) => {
   const { channel, posts } = props;
 
   const CHANNEL_META = {
-    title: `${channel.channel_name} x Kontenku - Exclusive Content for Subscribers`,
-    description: `${channel.channel_name} x Kontenku - Tempat ${channel.channel_name} menawarkan konten eksklusif yang dapat dibayar dengan harga premium. Nikmati video dan musik yang Anda sukai, unggah konten original, dan bagikan semuanya untuk para fans.`,
+    title: `${channel.channel_name} x Kontenku Langganan Konten Terbaik - Nikmati Konten Berkualitas Tinggi Setiap Hari`,
+    description: `${channel.channel_name} x Kontenku - Nikmati konten berkualitas tinggi setiap hari dengan berlangganan konten kami. Terupdate, informatif, dan menyenangkan, langganan konten kami adalah pilihan terbaik bagi Anda.`,
   };
 
   return <MetaHead meta={CHANNEL_META} />;
